f1f0b83f83f1984d17fc01d262b463752cf71b98
osmanmm <mohamed.osman@outlook.com>
2021-06-20 04:44:27 +0000

    function odd&even number

---
+++ odd.js
@@ -0,0 +1,9 @@
+const isOdd = function (n) {
+  if (n % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
